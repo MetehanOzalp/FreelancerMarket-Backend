72ec20b50c212d64c015a3f33c5becd7d02d5776
Metehan Özalp <metehanozalp@hotmail.com>
2021-11-03 18:33:20 +0300

    Initial commit

---
+++ src/main/java/GraduationProject/freelancermarket/FreelancermarketApplication.java
@@ -0,0 +1,13 @@
+package GraduationProject.freelancermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelancermarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelancermarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GraduationProject/freelancermarket/FreelancermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package GraduationProject.freelancermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelancermarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
