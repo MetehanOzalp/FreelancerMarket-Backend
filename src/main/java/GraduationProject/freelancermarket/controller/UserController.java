47799e23690525b5c12c8ea1a367d1fdf42f3f44
MetehanOzalp <metehanozalp@hotmail.com>
2022-06-08 00:30:41 +0300

    Get user images by user names

--- src/main/java/GraduationProject/freelancermarket/controller/UserController.java
+++ src/main/java/GraduationProject/freelancermarket/controller/UserController.java
@@ -1,8 +1,13 @@
 package GraduationProject.freelancermarket.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +25,16 @@ public class UserController {
 	@GetMapping("getByUserName")
 	public ResponseEntity<?> getByUserName(@RequestParam String userName) {
 		var result = userService.getByUserName(userName);
+		if (!result.isSuccess()) {
+			return new ResponseEntity<Object>(result, HttpStatus.BAD_REQUEST);
+		}
+		return ResponseEntity.ok(result);
+	}
+
+	@PostMapping("getUserImages")
+	@PreAuthorize("hasRole('ROLE_EMPLOYER')" + "|| hasRole('ROLE_FREELANCER')")
+	public ResponseEntity<?> getUserImages(@RequestBody List<String> userNames) {
+		var result = userService.getUserImages(userNames);
 		if (!result.isSuccess()) {
 			return new ResponseEntity<Object>(result, HttpStatus.BAD_REQUEST);
 		}
